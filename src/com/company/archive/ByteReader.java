acedc1f0545f2bed652d2899545f6290ab0d2730
Skulblako <Skulblako@gmail.com>
2020-04-14 21:09:31 +0300

    Add 1 example

---
+++ src/com/company/archive/ByteReader.java
@@ -0,0 +1,14 @@
+package com.company.archive;
+
+import java.io.IOException;
+
+public class ByteReader {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println(System.in.read());
+        }
+
+        // 10 - enter
+        // 32 - space
+    }
+}
